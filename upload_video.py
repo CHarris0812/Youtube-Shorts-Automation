b8e86bf3ca4002465dd469d27ae729d18ffea28b
Connor Harris <CWHarris0812@gmail.com>
2024-02-12 18:00:02 -0600

    Restructured as class

--- upload_video.py
+++ upload_video.py
@@ -3,66 +3,78 @@
 
 import httplib2
 import os
-import sys
 
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
+from argparse import Namespace
 
+class videoUploader():
+  args = Namespace()
 
-def get_authenticated_service(args):
-  flow = flow_from_clientsecrets("client_secrets.json", scope="https://www.googleapis.com/auth/youtube.upload")
+  def __init__(self):
+    self.args.auth_host_name = 'localhost'
+    self.args.noauth_local_webserver = False
+    self.args.auth_host_port = [8080, 8090]
+    self.args.logging_level="ERROR"
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+  def upload(self, file, title = "Untitled Video", description = "", category = "22", keywords = "", privacyStatus = "public"):
+    if not os.path.exists(file):
+      exit("Invalid File")
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
+    self.args.file = file
+    self.args.title = title
+    self.args.description = description
+    self.args.category = category
+    self.args.keywords = keywords
+    self.args.privacyStatus = privacyStatus 
 
-  return build("youtube", "v3", http=credentials.authorize(httplib2.Http()))
+    print(self.args) 
+    youtube = self.get_authenticated_service()
+    self.initialize_upload(youtube)
 
-def initialize_upload(youtube, options):
-  tags = None
-  if options.keywords:
-    tags = options.keywords.split(",")
+  def get_authenticated_service(self):
+    flow = flow_from_clientsecrets("client_secrets.json", scope="https://www.googleapis.com/auth/youtube.upload")
 
-  body=dict(
-    snippet=dict(
-      title=options.title,
-      description=options.description,
-      tags=tags,
-      categoryId=options.category
-    ),
-    status=dict(
-      privacyStatus=options.privacyStatus
+    storage = Storage("%s-oauth2.json" % "upload_video.py")
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+      credentials = run_flow(flow, storage, self.args)
+
+    return build("youtube", "v3", http=credentials.authorize(httplib2.Http()))
+
+  def initialize_upload(self, youtube):
+    tags = None
+    if self.args.keywords:
+      tags = self.args.keywords.split(",")
+
+    body=dict(
+      snippet=dict(
+        title=self.args.title,
+        description=self.args.description,
+        tags=tags,
+        categoryId=self.args.category
+      ),
+      status=dict(
+        privacyStatus=self.args.privacyStatus
+      )
     )
-  )
 
-  # Upload video
-  insert_request = youtube.videos().insert(
-    part=",".join(list(body.keys())),
-    body=body,
-    media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
-  )
+    # Upload video
+    insert_request = youtube.videos().insert(
+      part=",".join(list(body.keys())),
+      body=body,
+      media_body=MediaFileUpload(self.args.file, chunksize=-1, resumable=True)
+    )
 
-  print("Upload Started")
-  insert_request.next_chunk()
-  print("File Uploaded")
+    print("Upload Started")
+    insert_request.next_chunk()
+    print("File Uploaded")
 
 #Parse arguments
 if __name__ == '__main__':
-  argparser.add_argument("--file", required=True)
-  argparser.add_argument("--title", default="Test Title")
-  argparser.add_argument("--description", default="Test Description")
-  argparser.add_argument("--category", default="22")
-  argparser.add_argument("--keywords", default="")
-  argparser.add_argument("--privacyStatus", choices=("public", "private"), default="public")
-  args = argparser.parse_args()
-
-  if not os.path.exists(args.file):
-    exit("Invalid File")
-
-  youtube = get_authenticated_service(args)
-  initialize_upload(youtube, args)
+  myUploader = videoUploader()
+  myUploader.upload("Test.mp4", title="Testing as class")
