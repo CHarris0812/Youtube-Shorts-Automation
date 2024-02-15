f7ce588d7102b0e8477841e7a0d43bd101d11dae
Connor Harris <CWHarris0812@gmail.com>
2024-02-14 20:20:24 -0600

    set up structure

---
+++ generate_video.py
@@ -0,0 +1,15 @@
+class videoGenerator():
+    def __init__():
+        print("initialized")
+
+    def createVideo(self):
+        return
+    
+    def renderVideo(self):
+        return
+    
+    def saveVideo(self, video):
+        return
+
+if __name__ == "__main__":
+    myGenerator = videoGenerator()
